4cac95aa49ae9898b94e2f3b8fd22a974370f27f
skyscraper4 <jcardenas1@ucol.mx>
2020-11-25 15:25:44 -0600

    Implemented esPar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 export default class App {
  
+    esPar(numero) {
+    numero = numero % 2;
+     if (numero == 0) {
+         return true;
+     } else {
+         return false;
+     }   
+    }
+    
 }
 
 let app = new App();
+
+console.log("Probando: esPar");
+console.log(app.esPar(444));
+console.log(app.esPar(5));
