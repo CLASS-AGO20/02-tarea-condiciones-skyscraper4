ff65f4d9f7dbd192f01e76fd87417a8533a9749e
skyscraper4 <jcardenas1@ucol.mx>
2020-11-25 23:48:35 -0600

    Implemented obtenerCalificacion

--- src/main.js
+++ src/main.js
@@ -34,6 +34,25 @@ export default class App {
      return sueldo;
        } 
     
+obtenerCalificacion(calificacionnumerica) {
+    let notaFinal;
+    if (calificacionnumerica == 0 || calificacionnumerica ==1) {
+        notaFinal = "C";
+    } else if (calificacionnumerica == 2 || calificacionnumerica == 3) {
+        notaFinal = "C";
+    } else if (calificacionnumerica == 4 || calificacionnumerica == 5) {
+        notaFinal = "C";
+    } else if (calificacionnumerica == 6 || calificacionnumerica == 7) {
+        notaFinal = "B";
+    } else if (calificacionnumerica == 8 || calificacionnumerica == 9) {
+        notaFinal = "A";
+    } else if (calificacionnumerica == 10) {
+        notaFinal = "A+";
+    } else {
+        notaFinal = "ERROR";
+    }
+    return notaFinal;
+}
 
 }
 
@@ -53,3 +72,10 @@ console.log(app.calcularSalario(1, 8));
 console.log(app.calcularSalario(2, 8));
 console.log(app.calcularSalario(3, 8));
 console.log(app.calcularSalario(4, 8));
+
+console.log("probando obtenerCalificacion");
+console.log(app.obtenerCalificacion(3));
+console.log(app.obtenerCalificacion(6));
+console.log(app.obtenerCalificacion(8));
+console.log(app.obtenerCalificacion(10));
+console.log(app.obtenerCalificacion(14));
