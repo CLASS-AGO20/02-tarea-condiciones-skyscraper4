bd76299d12c60a5a91347b40b8d47435b65c16b6
skyscraper4 <jcardenas1@ucol.mx>
2020-11-25 23:14:10 -0600

    Implemented esMultiplo

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ export default class App {
      }  
     }
     
+    esMultiplo(numero1, numero2) {
+     if (numero1 % numero2 == 0) {
+         return true;
+     } else {
+         return false;
+     }   
+    }
 }
 
 let app = new App();
@@ -18,3 +25,7 @@ console.log("Probando: esPar");
 console.log(app.esPar(444));
 console.log(app.esPar(5));
 console.log(app.esPar(0))
+
+console.log("probando: esMultiplo");
+console.log(app.esMultiplo(16, 4));
+console.log(app.esMultiplo(9, 4));
